1e7df823f349d08bf933045b404085e7ee78e6a0
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-08-03 12:30:50 -0400

    fixed mispelling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import jumboData from './fixtures/jumbo';
-import Jumbotron from './comonents/jumbotron/index';
+import Jumbotron from './components/jumbotron/index';
 
 export default function App() {
   return (
